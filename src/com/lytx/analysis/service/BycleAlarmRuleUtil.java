3f176f919f6df894a0f83550a19bdafeab8ab6b5
xietf1983 <396980588@qq.com>
2017-02-06 08:56:41 +0800

    d

--- src/com/lytx/analysis/service/BycleAlarmRuleUtil.java
+++ src/com/lytx/analysis/service/BycleAlarmRuleUtil.java
@@ -63,64 +63,107 @@ public class BycleAlarmRuleUtil {
 		// boolean ret = false;
 		buildRules();
 		boolean ret = false;
-		//iLog.error("rules-list" + rules);
-		TrackBycleShort tshort = ElectrombileServiceUtil.getService().getTrackBycleShort(by.getFdId());
-		boolean candelete = true;
-		if (ElectrombileServiceUtil.getService().findBycleWhite(by)) {
-			//iLog.error("白名单" + by.getFdId());
-			BycleAlarmRuleServiceUtil.addbycleAlarmPreDealHis(by, null, true);
-			return false;
-		}
-		if (by.getAreaId() != null) {
-			String firstAreaId = BycleAlarmRuleServiceUtil.getFirstAreaId(by.getAreaId());
-			List<String> list = BycleAlarmRuleServiceUtil.getRules(firstAreaId);
-			if (list != null && list.size() > 0) {
-				for (String s : list) {
-					RuleModel r = rules.get(s);
-					if (r != null && r.getType() == 1) {
-						// 获取24小时前的异常过车数量
-						int countValue = BycleAlarmRuleServiceUtil.getWaringCountLastDay(by);
-						if (countValue > r.getWaringCount()) {
-							ret = true;
-							BycleAlarmRuleServiceUtil.addbycleAlarmPreDealHis(by, r.getRuleId(), true);
-							break;
-						}
-
-					}
-					if (r != null && r.getType() == 2) {
-						long a = r.getIncomeingTime();
-						Date end = new Date(by.getCreateDate().getTime() + a * 60 * 1000);
-						if (now.getTime() > end.getTime()) {
-							boolean b = BycleAlarmRuleServiceUtil.judgementNormalAlfer(DateUtil.toString(by.getCreateDate()), DateUtil.toString(end), by);
-							if (b) {
-								BycleAlarmRuleServiceUtil.addbycleAlarmPreDealHis(by, r.getRuleId(), true);
+		// iLog.error("rules-list" + rules);
+		try {
+			TrackBycleShort tshort = ElectrombileServiceUtil.getService().getTrackBycleShort(by.getFdId());
+			boolean candelete = true;
+			if (ElectrombileServiceUtil.getService().findBycleWhite(by)) {
+				iLog.error("白名单" + by.getFdId());
+				BycleAlarmRuleServiceUtil.addbycleAlarmPreDealHis(by, null, true);
+				return false;
+			}
+			if (by.getAreaId() != null) {
+				String firstAreaId = BycleAlarmRuleServiceUtil.getFirstAreaId(by.getAreaId());
+				List<String> list = BycleAlarmRuleServiceUtil.getRules(firstAreaId);
+				if (list != null && list.size() > 0) {
+					for (String s : list) {
+						RuleModel r = rules.get(s);
+						if (r != null && r.getType() == 1) {
+							// 获取24小时前的异常过车数量
+							int countValue = BycleAlarmRuleServiceUtil.getWaringCountLastDay(by);
+							if (countValue > r.getWaringCount()) {
 								ret = true;
+								BycleAlarmRuleServiceUtil.addbycleAlarmPreDealHis(by, r.getRuleId(), true);
 								break;
 							}
-						} else {
-							candelete = false;
-							continue;
+
 						}
+						if (r != null && r.getType() == 2) {
+							long a = r.getIncomeingTime();
+							Date end = new Date(by.getCreateDate().getTime() + a * 60 * 1000);
+							if (now.getTime() > end.getTime()) {
+								boolean b = BycleAlarmRuleServiceUtil.judgementNormalAlfer(DateUtil.dateToString(by.getCreateDate()), DateUtil.dateToString(end), by);
+								if (b) {
+									BycleAlarmRuleServiceUtil.addbycleAlarmPreDealHis(by, r.getRuleId(), true);
+									ret = true;
+									break;
+								}
+							} else {
+								candelete = false;
+								continue;
+							}
+						}
+
+					}
+					if (candelete && !ret) {
+						BycleBlack b = new BycleBlack();
+						BycleInfoShort bycleInfoShort = ElectrombileServiceUtil.getService().getBycleInfoShort(by.getFdId());
+						if (bycleInfoShort != null) {
+							by.setBycleOwner(bycleInfoShort.getOwner());
+							by.setUserTel(bycleInfoShort.getUserTel());
+						}
+						// by.setAlarmId(SequenceGeneratorServiceUtil.getSequenceNext(SequenceGeneratorServiceUtil.bycleseqName));
+						b.setActNo(by.getActNo());
+						b.setAlarmId(by.getAlarmId());
+						b.setAlarmMemo(by.getAlarmMemo());
+						b.setAlarmTime(by.getAlarmTime());
+						b.setAlarmType(by.getAlarmType());
+						BycleStationModel stationModel = ElectrombileServiceUtil.getService().getBycleStationModel(by.getStationId());
+						if (stationModel != null) {
+							by.setAreaCode(stationModel.getAreaCode());
+						}
+						b.setAreaCode(by.getAreaCode());
+						b.setAreaId(by.getAreaId());
+						b.setBycleOwner(by.getBycleOwner());
+						b.setCaseId(ElectrombileServiceUtil.getService().getCaseIdNext(by.getAreaCode()));
+						b.setRuleId(UUID.randomUUID().toString());
+						b.setBycleId(by.getBycleid());
+						b.setFdId(by.getFdId());
+						b.setPlatenoArea(by.getPlatenoArea());
+						b.setPlateNo(by.getPlateNo());
+						b.setBycleOwner(by.getBycleOwner());
+						b.setAreaCode(by.getAreaCode());
+						b.setStatus("0");
+						b.setIssecret(0);
+						b.setSouce("4");
+						b.setBycleOwner(by.getBycleOwner());
+						b.setAlarmPhone(by.getUserTel());
+						b.setType(0);// 异常车辆库
+						ElectrombileServiceUtil.getService().addBycleBlack(b, null);
+						by.setCaseId(b.getCaseId());
+						by.setRuleId(b.getRuleId());
+						by.setType(0);
+						by.setStatus("0");
+						ElectrombileServiceUtil.getService().addBycleHandleAlarm(by);
+						BycleAlarmRuleServiceUtil.addbycleAlarmPreDealHis(by, null, true);
 					}
 
-				}
-				if (candelete && !ret) {
+				} else {
 					BycleBlack b = new BycleBlack();
 					BycleInfoShort bycleInfoShort = ElectrombileServiceUtil.getService().getBycleInfoShort(by.getFdId());
 					if (bycleInfoShort != null) {
 						by.setBycleOwner(bycleInfoShort.getOwner());
 						by.setUserTel(bycleInfoShort.getUserTel());
 					}
-					//by.setAlarmId(SequenceGeneratorServiceUtil.getSequenceNext(SequenceGeneratorServiceUtil.bycleseqName));
+					BycleStationModel stationModel = ElectrombileServiceUtil.getService().getBycleStationModel(by.getStationId());
+					if (stationModel != null) {
+						by.setAreaCode(stationModel.getAreaCode());
+					}
 					b.setActNo(by.getActNo());
 					b.setAlarmId(by.getAlarmId());
 					b.setAlarmMemo(by.getAlarmMemo());
 					b.setAlarmTime(by.getAlarmTime());
 					b.setAlarmType(by.getAlarmType());
-					BycleStationModel stationModel =ElectrombileServiceUtil.getService().getBycleStationModel(by.getStationId());
-					if(stationModel!= null){
-						by.setAreaCode(stationModel.getAreaCode());
-					}
 					b.setAreaCode(by.getAreaCode());
 					b.setAreaId(by.getAreaId());
 					b.setBycleOwner(by.getBycleOwner());
@@ -145,52 +188,13 @@ public class BycleAlarmRuleUtil {
 					by.setStatus("0");
 					ElectrombileServiceUtil.getService().addBycleHandleAlarm(by);
 					BycleAlarmRuleServiceUtil.addbycleAlarmPreDealHis(by, null, true);
+
 				}
 
 			} else {
-				BycleBlack b = new BycleBlack();
-				BycleInfoShort bycleInfoShort = ElectrombileServiceUtil.getService().getBycleInfoShort(by.getFdId());
-				if (bycleInfoShort != null) {
-					by.setBycleOwner(bycleInfoShort.getOwner());
-					by.setUserTel(bycleInfoShort.getUserTel());
-				}
-				BycleStationModel stationModel =ElectrombileServiceUtil.getService().getBycleStationModel(by.getStationId());
-				if(stationModel!= null){
-					by.setAreaCode(stationModel.getAreaCode());
-				}
-				b.setActNo(by.getActNo());
-				b.setAlarmId(by.getAlarmId());
-				b.setAlarmMemo(by.getAlarmMemo());
-				b.setAlarmTime(by.getAlarmTime());
-				b.setAlarmType(by.getAlarmType());
-				b.setAreaCode(by.getAreaCode());
-				b.setAreaId(by.getAreaId());
-				b.setBycleOwner(by.getBycleOwner());
-				b.setCaseId(ElectrombileServiceUtil.getService().getCaseIdNext(by.getAreaCode()));
-				b.setRuleId(UUID.randomUUID().toString());
-				b.setBycleId(by.getBycleid());
-				b.setFdId(by.getFdId());
-				b.setPlatenoArea(by.getPlatenoArea());
-				b.setPlateNo(by.getPlateNo());
-				b.setBycleOwner(by.getBycleOwner());
-				b.setAreaCode(by.getAreaCode());
-				b.setStatus("0");
-				b.setIssecret(0);
-				b.setSouce("4");
-				b.setBycleOwner(by.getBycleOwner());
-				b.setAlarmPhone(by.getUserTel());
-				b.setType(0);// 异常车辆库
-				ElectrombileServiceUtil.getService().addBycleBlack(b, null);
-				by.setCaseId(b.getCaseId());
-				by.setRuleId(b.getRuleId());
-				by.setType(0);
-				by.setStatus("0");
-				ElectrombileServiceUtil.getService().addBycleHandleAlarm(by);
 				BycleAlarmRuleServiceUtil.addbycleAlarmPreDealHis(by, null, true);
-
 			}
-
-		}else{
+		} catch (Exception ex) {
 			BycleAlarmRuleServiceUtil.addbycleAlarmPreDealHis(by, null, true);
 		}
 
